d0b6aa7fa8174e28a0f8aa278a76a3068d679a8d
GabrielFerreiraTSI <gabrielferreirati9@gmail.com>
2026-03-31 23:12:43 -0300

    Preperando deploy no Render.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,9 +14,15 @@ jwt = JWTManager()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL",
+        "postgresql://gabriel:1234@localhost/tarefas_db"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = os.getenv(
+        "JWT_SECRET_KEY",
+        "chave_local_desenvolvimento"
+    )
 
     db.init_app(app)
     migrate.init_app(app, db)
